74157c2acad05e590649fc51c95fd2066e1cc01d
bhanupriya <cs100priya@gmail.com>
2020-01-30 17:30:44 +0530

    admin pannel update

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Question,Choice
 
+class ChoiceAdmin(admin.ModelAdmin):
+	list_display=("question","choice_text","votes")
+
+admin.site.register(Choice,ChoiceAdmin)
+admin.site.register(Question)
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
